6604c5d84870ef383de77939de18c1c452bfb7f8
Kiet Lac <kietlullk6969@gmail.com>
2024-01-25 12:27:33 +0700

    add json file

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,5 +6,9 @@ const port = 1000
 app.use(express.json())
 
 app.use('/users', usersRouter)
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+  })
 app.listen(port, () => console.log(`Server running on port ${port}`))
 databaseService._connect()
