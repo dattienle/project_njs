21ab3bcbe118b68feefda4bb5e5c3934193bdda9
anhkiet307 <kietlulk6969@gmail.com>
2024-02-26 12:53:35 +0700

    fix connect mongo

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use('/categories', categoriesRouter);
 app.use('/notifications', notificationRouter);
 
-mongoose.connect("mongodb://datltse160245:letiendat2002@sdn301m.a5pjj2z.mongodb.net/njs", {useMongoClient:true});
+mongoose.connect("mongodb+srv://datltse160245:letiendat2002@sdn301m.a5pjj2z.mongodb.net/njs", {useMongoClient:true});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
